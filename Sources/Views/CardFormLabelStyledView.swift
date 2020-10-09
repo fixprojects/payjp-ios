9f6f610a060050f758b0472f5df0c131455f49da
fixprojects <72069478+fixprojects@users.noreply.github.com>
2020-10-09 17:51:53 +0900

    allow visible cardNumber, expiration

--- Sources/Views/CardFormLabelStyledView.swift
+++ Sources/Views/CardFormLabelStyledView.swift
@@ -29,8 +29,8 @@ public class CardFormLabelStyledView: CardFormView, CardFormProperties {
     @IBOutlet weak var holderContainer: UIStackView!
     @IBOutlet weak var ocrButton: UIButton!
 
-    @IBOutlet weak var cardNumberTextField: FormTextField!
-    @IBOutlet weak var expirationTextField: FormTextField!
+    @IBOutlet weak internal cardNumberTextField: FormTextField!
+    @IBOutlet weak internal expirationTextField: FormTextField!
     @IBOutlet weak var cvcTextField: FormTextField!
     @IBOutlet weak var cardHolderTextField: FormTextField!
 
